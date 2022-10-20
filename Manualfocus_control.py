29a8e6897eea48e09f734e9d6e8beb97628a80de
tom-ard <tom@arducam.com>
2022-10-20 15:34:38 +0800

    Add manual focus example.

---
+++ Manualfocus_control.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# To run this example, update libcamera-dev to version 0.0.10.
+
+import time
+
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
+
+picam2.start()
+time.sleep(1)
+
+# AfMode: Set the AF mode (manual, auto, continuous)
+# LensPosition: Manual focus, Set the lens position.
+picam2.set_controls({"AfMode": 0, "LensPosition": 100.0})
+time.sleep(5)
+
+picam2.set_controls({"LensPosition": 500.0})
+
+time.sleep(5)
